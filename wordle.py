029e4a6035e3cc010a905d8b6d7f18d737f43d52
poolpurr <25ltwigg@cvsdstudents.org>
2024-01-04 11:33:40 -0500

    mildy broken

--- wordle.py
+++ wordle.py
@@ -8,21 +8,38 @@ def errorMsg(enter): #@ defines a function for an error message
         enter = input("Choose a 5 letter word:")
         errorMsg(enter)
         
+def gameLoop():
+    print("The first player will choose a five letter word")
+    word = input("Choose a 5 letter word:")
+
+    errorMsg(word) #calls the error message
+
+    print("\nNow player 2 enter a 5 letter word as a guess")
+    guess = ""
+    i = 0
+    found = ["Not found", "Not found", "Not found", "Not found", "Not found"]
+    while guess != word or i > 6:
+        guess = input("Enter a 5 letter word:")
+        errorMsg(guess) #calls the error message
+        for i in range(5):
+            if guess[i] == word[i]:
+                print("The letter", guess[i], "matches letter in correct spot")
+                found[i] = "Found"
+            if guess[i] != word[i]:
+                yellow = False
+                for j in range(len(word)):
+                    if guess[i] == word[j] and found[j] == "Not found":
+                        print("The letter", guess[i], "is contained within the word")
+                        yellow = True
+                        found[j] = "Yellow"
+                        break
+                if not yellow:
+                    print("No Match")
+        i += 1
+
 print("Welcome to wordle")
-print("The first player will choose a five letter word")
-word = input("Choose a 5 letter word:")
-
-errorMsg(word) #calls the error message
-
-print("\nNow player 2 enter a 5 letter word as a guess")
-guess = input("Enter a 5 letter word:")
-
-errorMsg(guess) #calls the error message
-
-for i in range(5):
-    if guess[i] == word[i]:
-        print("The letter", guess[i], "matches letter in correct spot")
-    if guess[i] != word[i]:
-        print("No Match")
-        continue
-    
+gameLoop()
+print("Would you like to play again with a diffrent word (YES or NO)")
+answer = input("Answer:")
+if answer == "YES":
+    gameLoop()
