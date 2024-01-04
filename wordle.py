8c17f4826a494e3f1480a74932d100b31b9aacd1
poolpurr <25ltwigg@cvsdstudents.org>
2024-01-04 11:15:16 -0500

    part of game

--- wordle.py
+++ wordle.py
@@ -1,14 +1,28 @@
-from random import *
-
+def errorMsg(enter): #@ defines a function for an error message
+    if not enter.isalpha(): #Error message if word is not within alphabet
+        print("The word must contain all letters, try again\n")
+        enter = input("Choose a 5 letter word:")
+        errorMsg(enter)
+    if len(enter) != 5 :  # Error message if word is not 5 letters
+        print("The word must be 5 characters, try again\n")
+        enter = input("Choose a 5 letter word:")
+        errorMsg(enter)
+        
 print("Welcome to wordle")
 print("The first player will choose a five letter word")
 word = input("Choose a 5 letter word:")
 
-while not word.isalpha():
-    print("The word must contain all letters")
-    word = input("Choose a 5 letter word:")
-while len(word) > 5 or len(word) < 5:
-    print("The word must be 5 characters")
-    word = input("Choose a 5 letter word:")
+errorMsg(word) #calls the error message
 
-print(word)
+print("\nNow player 2 enter a 5 letter word as a guess")
+guess = input("Enter a 5 letter word:")
+
+errorMsg(guess) #calls the error message
+
+for i in range(5):
+    if guess[i] == word[i]:
+        print("The letter", guess[i], "matches letter in correct spot")
+    if guess[i] != word[i]:
+        print("No Match")
+        continue
+    
