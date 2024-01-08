fe97a8fefdc01cd702f6b93f57f760c2bdf7b163
poolpurr <25ltwigg@cvsdstudents.org>
2024-01-08 11:07:15 -0500

    checking yellow

--- wordle.py
+++ wordle.py
@@ -17,21 +17,19 @@ def gameLoop():
     print("\nNow player 2 enter a 5 letter word as a guess")
     guess = ""
     i = 0
-    found = ["Not found", "Not found", "Not found", "Not found", "Not found"]
+ 
     while guess != word or i > 6:
         guess = input("Enter a 5 letter word:")
         errorMsg(guess) #calls the error message
         for i in range(5): #runs this sequence once for each letter
             if guess[i] == word[i]: # if the guess is the same letter as the word print that
                 print("The letter", guess[i], " in spot ", str(i + 1), "matches letter in correct spot")
-                found[i] = "Found"
             elif guess[i] != word[i]: #if the letter of the guess is not equal to the letter of the letter then check if yellow
                 yellow = False
                 for j in range(len(word)):
-                    if guess[i] == word[j] and found[j] == "Not found":
+                    if guess[i] == word[j and word[j] != guess[j]]:
                         print("The letter", guess[i], "is contained within the word")
                         yellow = True
-                        found[j] = "Yellow"
                         break
                 if not yellow: # if it is not yellow
                     print("No Match")
@@ -43,3 +41,4 @@ print("Would you like to play again with a diffrent word (YES or NO)")
 answer = input("Answer:")
 if answer == "YES":
     gameLoop()
+     
